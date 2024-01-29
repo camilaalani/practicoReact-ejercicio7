1f5c82c9ae2aa4f2abea6f81a74cfcd786833a73
Camila Alani <alanicamila124@gmail.com>
2024-01-29 01:20:00 -0300

    agregue un section y divs para las filas y columnas

--- src/components/EmpleadoList.jsx
+++ src/components/EmpleadoList.jsx
@@ -3,12 +3,16 @@ import EmpleadoRow from "./EmpleadoRow";
 
 const EmpleadoList = ({empleados}) => {
     return (
-        <section>
-            <ListGroup>
+        <section className="container my-3">
+            <div className="row justify-content-center">
+                <div className="col col-md-6">
+                    <ListGroup>
                         {empleados.map((empleados) => (
                             <EmpleadoRow key={empleados.id} empleados={empleados}></EmpleadoRow>
                         ))}
                     </ListGroup>
+                </div>
+            </div>
         </section>
     );
 };
